8149803bd7c67d2dbd1de9483ac03acad6145819
Kevin Jing <kevinhigher@gmail.com>
2014-01-29 23:11:59 +0800

    Add JS example High Performance Loadding

---
+++ JS/examples/High Performance/Loadding/test1.js
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: HSZM1106-0312
+ * Date: 14-1-28
+ * Time: 下午10:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+function callback() {
+    alert("Script has loaded!");
+}
+
+
+var Application = {};
+Application.init = function(){
+    alert("Init Application");
+}
